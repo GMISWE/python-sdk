4ef070e26ab7a0ebfce113cd03ba89842b54efe3
yijiehong <yijie.h@gmitec.net>
2025-04-19 18:08:08 -0700

    Support vllm and SGlang command with custom model

--- examples/inference_task_with_custom_model.py
+++ examples/inference_task_with_custom_model.py
@@ -30,46 +30,53 @@ print(f"Found {len(templates)} templates: {templates}")
 
 # Create an artifact from a template
 # picked_template_name = "GMI_inference_template"
-# picked_template_name = "GMI_SGLang_0.4.5_template"
-# serve_command = "python3 -m sglang.launch_server --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --trust-remote-code --mem-fraction-static 0.8 --tp 1"
 
-# artifact_id, recommended_replica_resources = cli.artifact_manager.create_artifact_from_template_name(
-#     artifact_template_name=picked_template_name,
-#     env_parameters={
-#         "SERVER_COMMAND": serve_command,
-#         "GPU_TYPE": "H100",
-#     }
-# )
-# print(f"Created artifact {artifact_id} with recommended resources: {recommended_replica_resources}")
+# Example for vllm server
+# picked_template_name = "vllm_0.8.4_test10"
+# serve_command = "vllm serve deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --trust-remote-code --gpu-memory-utilization 0.8"
 
-# # Upload model files to artifact
-# cli.artifact_manager.upload_model_files_to_artifact(artifact_id, model_checkpoint_save_dir)
+# Example for sglang server
+picked_template_name = "SGLang_0.4.5_test7"
+serve_command = "python3 -m sglang.launch_server --model-path deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --trust-remote-code --mem-fraction-static 0.8"
+artifact_name = "artifact_sglang_hello_world"
 
-# # Create Task based on Artifact
-# new_task = Task(
-#     config=TaskConfig(
-#         ray_task_config=RayTaskConfig(
-#             artifact_id=artifact_id,
-#             file_path="serve",
-#             deployment_name="app",
-#             replica_resource=recommended_replica_resources,
-#         ),
-#         task_scheduling = TaskScheduling(
-#             scheduling_oneoff=OneOffScheduling(
-#                 trigger_timestamp=int(datetime.now().timestamp()),
-#                 min_replicas=1,
-#                 max_replicas=4,
-#             )
-#         ),
-#     ),
-# )
-# task = cli.task_manager.create_task(new_task)
-# task_id = task.task_id
-# task = cli.task_manager.get_task(task_id)
-# print(f"Task created: {task.config.task_name}. You can check details at https://inference-engine.gmicloud.ai/user-console/task")
+artifact_id, recommended_replica_resources = cli.artifact_manager.create_artifact_for_serve_command_and_custom_model(
+    template_name=picked_template_name,
+    artifact_name=artifact_name,
+    serve_command=serve_command,
+    gpu_type="H100",
+    artifact_description="This is a test artifact",
+)
+print(f"Created artifact {artifact_id} with recommended resources: {recommended_replica_resources}")
 
-# # Start Task and wait for it to be ready
-# cli.task_manager.start_task_and_wait(task_id)
+# Upload model files to artifact
+cli.artifact_manager.upload_model_files_to_artifact(artifact_id, model_checkpoint_save_dir)
 
-# # Call chat completion
-# print(call_chat_completion(cli, task_id))
+# Create Task based on Artifact
+new_task = Task(
+    config=TaskConfig(
+        ray_task_config=RayTaskConfig(
+            artifact_id=artifact_id,
+            file_path="serve",
+            deployment_name="app",
+            replica_resource=recommended_replica_resources,
+        ),
+        task_scheduling = TaskScheduling(
+            scheduling_oneoff=OneOffScheduling(
+                trigger_timestamp=int(datetime.now().timestamp()),
+                min_replicas=1,
+                max_replicas=4,
+            )
+        ),
+    ),
+)
+task = cli.task_manager.create_task(new_task)
+task_id = task.task_id
+task = cli.task_manager.get_task(task_id)
+print(f"Task created: {task.config.task_name}. You can check details at https://inference-engine.gmicloud.ai/user-console/task")
+
+# Start Task and wait for it to be ready
+cli.task_manager.start_task_and_wait(task_id)
+
+# Call chat completion
+print(call_chat_completion(cli, task_id))
